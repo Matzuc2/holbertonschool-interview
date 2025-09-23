07844197272b6f6f40eadcd3b2069b903fedfa5d
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2025-09-23 16:43:09 +0200

    Update docstring in 0-lockboxes.py to clarify module and function purpose

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
+"""
+Module: 0-lockboxes
+
+Ce module fournit une fonction pour déterminer si toutes les boîtes d'une liste peuvent être ouvertes.
+Chaque boîte peut contenir des clés permettant d'ouvrir d'autres boîtes.
+"""
+
 def canUnlockAll(boxes):
+    """
+    Détermine si toutes les boîtes peuvent être ouvertes.
+
+    Args:
+        boxes (list of list of int): Liste de boîtes, chaque boîte contient des clés (indices d'autres boîtes).
+
+    Returns:
+        bool: True si toutes les boîtes peuvent être ouvertes, sinon False.
+    """
     keys = []
     for i, boxe in enumerate(boxes):
         if i == 0:

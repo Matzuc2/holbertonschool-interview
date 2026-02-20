dde624e6f42f6879d987f90710dd28249c4d5a2b
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-02-20 13:22:26 +0100

    Enhance log processing to handle fewer than 10 lines and print status codes with file sizes

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -16,10 +16,10 @@ import sys
 def main():
     """Main function to process log entries."""
     lines = []
-
     try:
         for index, line in enumerate(sys.stdin):
             lines.append(line)
+    
             if (index % 10 == 0 and index != 0):
                 status_dict = {}
                 total_file_size = 0
@@ -30,6 +30,20 @@ def main():
                     else:
                         status_dict[status_code] += 1
                     file_size = line.split(' ')[8]
+                total_file_size += int(file_size) or 0
+                print(f"File size: {total_file_size}")
+                for key, value in sorted(status_dict.items()):
+                    print(f"{key}: {value}")
+        if (index < 10):
+            status_dict = {}
+            total_file_size = 0
+            for line in lines:
+                status_code = line.split(' ')[7]
+                if status_code not in status_dict:
+                    status_dict[status_code] = 1
+                else:
+                    status_dict[status_code] += 1
+                file_size = line.split(' ')[8]
                 total_file_size += int(file_size) or 0
                 print(f"File size: {total_file_size}")
                 for key, value in sorted(status_dict.items()):

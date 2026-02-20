f5e3017201f204491d2369111da7763d27dd86a4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-02-20 13:51:42 +0100

    Add status code counting and file size accumulation in log processing

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -35,6 +35,17 @@ def main():
                 print(f"File size: {total_file_size}")
                 for key, value in sorted(status_dict.items()):
                     print(f"{key}: {value}")
+        for line in lines:
+            status_code = line.split(' ')[7]
+            if status_code not in status_dict:
+                status_dict[status_code] = 1
+            else:
+                status_dict[status_code] += 1
+                file_size = line.split(' ')[8]
+                total_file_size += int(file_size) or 0
+            print(f"File size: {total_file_size}")
+            for key, value in sorted(status_dict.items()):
+                print(f"{key}: {value}")
         if (index < 10):
             status_dict = {}
             total_file_size = 0

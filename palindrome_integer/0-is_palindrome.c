ae3c4be8248755b5a53e5ddfa9f3ea4e563a5b08
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-10 13:46:45 +0200

    Refactor palindrome functions for improved readability and consistency

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
 #include "palindrome.h"
 
-unsigned long reverseDigits(unsigned long num){
+/**
+ * reverseDigits - reverses the digits of a number
+ * @num: number to reverse
+ *
+ * Return: reversed number
+ */
+unsigned long reverseDigits(unsigned long num)
+{
     unsigned long rev_num = 0;
-    while (num > 0) {
+
+    while (num > 0)
+    {
         rev_num = rev_num * 10 + num % 10;
         num = num / 10;
     }
-    return(rev_num);
+    return (rev_num);
 }
 
-
-int is_palindrome(unsigned long n){
+/**
+ * is_palindrome - checks if a number is a palindrome
+ * @n: number to check
+ *
+ * Return: 1 if palindrome, 0 otherwise
+ */
+int is_palindrome(unsigned long n)
+{
     unsigned long reversed = reverseDigits(n);
-    if(reversed == n){
-        return(1);
-    }else{
-        return(0);
-    }
+
+    if (reversed == n)
+        return (1);
+    return (0);
 }

23e47744d5658e4f0968f97e01e5e6f6363f6ad4
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-10 14:21:08 +0200

    Enhance documentation for UTF-8 validation module and function

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""documented module"""
+
+
 def validUTF8(data):
+    """verify is a data of int is utf8 convertible"""
     try:
         byte_data = bytearray(data)
         return True

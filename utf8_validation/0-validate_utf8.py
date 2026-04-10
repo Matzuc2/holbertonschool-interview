bcff29837747e0e162b0e979b0390364cd596376
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-10 16:29:02 +0200

    Fix typo in docstring of validUTF8 function and improve validation logic

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -3,10 +3,12 @@
 
 
 def validUTF8(data):
-    """verify is a data of int is utf8 convertible"""
+    """verify if a data of int is utf8 convertible"""
     try:
         for d in data:
-            if d > 255:
+            bin_int = bin(d)
+            list_bin_str = list(bin_int)
+            if len(list_bin_str) > 9:
                 return False
         return True
     except ValueError:

fcc59568a861ec5a813e467bcc03305e2f05ffb0
Matzuc2 <mathieu.zucalli2@holbertonschool.com>
2026-04-17 16:34:52 +0200

    Add additional test cases for validUTF8 function to enhance coverage

--- utf8_validation/0-main.py
+++ utf8_validation/0-main.py
@@ -16,3 +16,6 @@ print(validUTF8(data))
 
 data =  [467, 133, 108]
 print(validUTF8(data))
+
+data = [240, 188, 128, 167]
+print(validUTF8(data))
--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,15 +6,15 @@ def validUTF8(data):
     """verify if a data of int is utf8 convertible"""
     for i, d in enumerate(data):
         binary_int = bin(d)
-        # print(f"{binary_int}, binary_int")
+        print(f"{binary_int}, binary_int")
         binary_str = split_binary_len9(binary_int)
-        # print(f"{binary_str}, binary_str")
+        print(f"{binary_str}, binary_str")
         decimal_int = int(binary_str, 2)
-        # print(f"{decimal_int} decimal int")
+        print(f"{decimal_int} decimal int")
         if decimal_int > 255:
             return False
         count_of_one = count_byte_expected_number(binary_str)
-        # print(count_of_one)
+        print(count_of_one)
         try:
             data[i + count_of_one]
         except IndexError:
@@ -52,10 +52,15 @@ def split_binary_len9(binary_str: str):
 def count_byte_expected_number(binary_str: str):
     count_of_one = 0
     list_binary = list(binary_str)
-    for i, number in enumerate(list_binary):
+    for i, number in enumerate(list_binary, 0):
         try:
-            if list_binary[i] == "0":
-                return count_of_one
+            if i == 0:
+                if list_binary[0] == "0":
+                    break
+                else:
+                    pass
+            elif list_binary[i] == "0":
+                break
             elif list_binary[i] == "1":
                 count_of_one += 1
         except IndexError:

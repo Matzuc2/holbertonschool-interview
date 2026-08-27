fdbfdc868b9c1e26af0d12f7cf2fd0c215545ca6
Matzuc2 <9793@holbertonstudents.com>
2026-08-27 17:46:48 +0200

    Add initial implementation of Star Wars API character retrieval

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+import process from 'process'
+
+async function GetMovieJsonContent(movieId){
+  const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    return result
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+async function GetCharactersFromUrls(urlList){
+
+}
+async function GetCharactersWithUrlFrom(json){
+    const characters_url = json["characters"]
+    const characterPromises = characters_url.map(async (character_url)=>{
+        try{
+            const response = await fetch(character_url)
+            if(!response.ok){
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const character = await response.json()
+            return character
+        }
+        catch (error) {
+            console.error(error.message);
+        }
+    })
+    const characters = Promise.all(characterPromises)
+    return characters
+}
+
+function GetCharacterNames(characters){
+    const characterNames = []
+    characters.map((character)=>{
+        const characterName = character["name"]
+        characterNames.push(characterName)
+    })
+    return characterNames
+}
+
+const movieId = process.argv[2]
+const movieContent = await GetMovieJsonContent(movieId)
+const characters_json = await GetCharactersWithUrlFrom(movieContent)
+const characterNames = GetCharacterNames(characters_json)
+
+characterNames.map((characterName)=>{
+    console.log(characterName)
+})
+
---
+++ starwars_api/README.md
@@ -0,0 +1,1 @@
+yeah star wars api

334dc73ad6ac71f9f5bfe3a18047b291cb97a5e1
Matzuc2 <9793@holbertonstudents.com>
2026-08-27 15:46:12 +0200

    Fix usage message formatting and refactor solve function placement in N-Queens solver

--- nqueens/0-nqueens.py
+++ nqueens/0-nqueens.py
@@ -4,7 +4,7 @@ import sys
 
 args = sys.argv
 if len(args) != 2:
-    print('Usage nqueens N')
+    print('Usage: nqueens N')
     exit(1)
 n = args[1]
 try:
@@ -18,6 +18,19 @@ if int(n) < 4:
     exit(1)
 
 
+def is_queen_safe(line, column, queens):
+    new_queen_pos = line + column
+    new_queen_neg = line - column
+    is_safe = True
+    for queen in queens:
+        queen_pos = queen[0] + queen[1]
+        queen_neg = queen[0] - queen[1]
+        if queen_neg == new_queen_neg or queen_pos\
+                == new_queen_pos or queen[1] == column:
+            is_safe = False
+    return is_safe
+
+
 def solve(line, n, queens):
     if line == n:
         print(queens)
@@ -29,17 +42,4 @@ def solve(line, n, queens):
             queens.pop()
 
 
-def is_queen_safe(line, column, queens):
-    new_queen_pos = line + column
-    new_queen_neg = line - column
-    is_safe = True
-    for queen in queens:
-        queen_pos = queen[0] + queen[1]
-        queen_neg = queen[0] - queen[1]
-        if queen_neg == new_queen_neg or queen_pos == new_queen_pos or queen[1] == column:
-            is_safe = False
-    return is_safe
-
-
 solve(0, int(n), [])
-

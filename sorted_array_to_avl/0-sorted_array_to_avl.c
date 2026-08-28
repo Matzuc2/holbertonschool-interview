905ee2aadb5c1e9d982c3ce747388d570ba3a4a4
Matzuc2 <9793@holbertonstudents.com>
2026-08-28 09:29:12 +0200

    Fix formatting inconsistencies by removing extra blank lines in sorted_array_to_avl.c

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "binary_trees.h"
- 
+
 /**
  * create_node - Creates a new binary tree node
  * @parent: Pointer to the parent node
@@ -11,7 +11,7 @@
 static binary_tree_t *create_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
- 
+
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
@@ -21,7 +21,7 @@ static binary_tree_t *create_node(binary_tree_t *parent, int value)
 	node->right = NULL;
 	return (node);
 }
- 
+
 /**
  * build_avl - Recursively builds an AVL tree from a sorted array
  * @parent: Pointer to the parent node
@@ -35,20 +35,20 @@ static avl_t *build_avl(avl_t *parent, int *array, int start, int end)
 {
 	avl_t *node;
 	int mid;
- 
+
 	if (start > end)
 		return (NULL);
- 
+
 	mid = (start + end) / 2;
 	node = create_node(parent, array[mid]);
 	if (!node)
 		return (NULL);
- 
+
 	node->left  = build_avl(node, array, start, mid - 1);
 	node->right = build_avl(node, array, mid + 1, end);
 	return (node);
 }
- 
+
 /**
  * sorted_array_to_avl - Builds an AVL tree from a sorted array
  * @array: Pointer to the first element of the sorted array
@@ -60,6 +60,6 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
 {
 	if (!array || size == 0)
 		return (NULL);
- 
+
 	return (build_avl(NULL, array, 0, (int)size - 1));
 }
